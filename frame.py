8c3673fad54c5d68291be3903bd3c26707620120
D Corbacho <dianacorbacho@gmail.com>
2019-02-07 09:39:54 +0000

    Update frame.py

--- frame.py
+++ frame.py
@@ -1,29 +1,39 @@
 import os
 import sys
 
-path = sys.argv[1]
-images = os.listdir(path)
-numbers = sorted(list(map(lambda image: image.partition('_')[2].partition('.')[0], images)))
+def getKey(item):
+    return int(item)
 
-first = numbers[0]
-last = numbers[len(numbers)- 1]
-
-if len(numbers) > 0:
-    print ("First index: " + first)
-    print ("Last index: " + last)
-
-strlen = len(first)
-
-index = int(first)
-missing = []
-
-for n in numbers:
-    if int(n) == index + 1:
-        missing.append(str(index).zfill(strlen))
+if len(sys.argv) < 2:
+    print('Error: missing path to images folder')
+else:
+    path = sys.argv[1]
+    images = os.listdir(path)
+    if len(sys.argv) == 3:
+        prefix = sys.argv[2]
+        numbers = sorted(list([image[len(prefix):].partition('.')[0] for image in images if image.startswith(prefix)]), key=getKey)
     else:
-        if int(n) > index:
-            missing.append('{min}-{max}'.format(min = str(index).zfill(strlen), max = str(int(n) - 1).zfill(strlen)))
-    index = int(n) + 1
+        numbers = sorted(list(map(lambda image: image.partition('_')[2].partition('.')[0], images)),key=getKey)
 
-print('Missing:')
-print(','.join(missing))
+    first = numbers[0]
+    last = numbers[len(numbers)- 1]
+
+    if len(numbers) > 0:
+        print ("First index: " + first)
+        print ("Last index: " + last)
+
+        strlen = len(first)
+
+        index = int(first)
+        missing = []
+
+        for n in numbers:
+            if int(n) == index + 1:
+                missing.append(str(index).zfill(strlen))
+        else:
+            if int(n) > index:
+                missing.append('{min}-{max}'.format(min = str(index).zfill(strlen), max = str(int(n) - 1).zfill(strlen)))
+                index = int(n) + 1
+
+                print('Missing:')
+                print(','.join(missing))
